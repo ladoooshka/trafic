6575ea288823bceb1db0d01514c0340752a3a684
ladoooshka <ladoooshka@mail.ru>
2021-10-07 14:57:22 +0300

    little changes in my bags

--- table_with_trafic_every_domen.py
+++ table_with_trafic_every_domen.py
@@ -23,7 +23,6 @@ def create_table(n_list):
 	return column
 
 table = create_table(list_with_test_data)
-pd_table = pd.DataFrame((table), column=['data', 'link', 'size'])
 
 def take_domen_name_second_level(table):
 	domen_name = []
@@ -31,10 +30,14 @@ def take_domen_name_second_level(table):
 	for row in table:
 		links.append(row[1])
 	for row in links:
-		part_of_link = row.split('.').split('/')
+		part_of_link = row.split('.')
 		len_part_of_link = len(part_of_link) - 1
-		for num_blok in range(len_of_part_link):
-			if part_of_link[num_blok] = 'com' or part_of_link[num_blok] = 'ru':
+		block_link = []
+		for num_blok in part_of_link:
+			num_blok.split('/')
+			block_link.append(num_blok)
+		for num_blok in range(len_part_of_link):	
+			if part_of_link[num_blok] == 'com' or part_of_link[num_blok] == 'ru':
 				name_domen = part_of_link[num_blok -1] +'.'+part_of_link[num_blok]
 				break
 		domen_name.append(name_domen)
@@ -53,7 +56,7 @@ def count_of_trafic_for_domen(data, domen):
 			if name_domen in link[1]:
 				count_trafic += link[2]
 		count_trafic /= (1024*3)
-		part_of trafic.append(name_domen)
+		part_of_trafic.append(name_domen)
 		part_of_trafic.append(round(count_trafic, 0))
 		list_for_count.append(part_of_trafic)
 	return list_for_count
@@ -67,11 +70,9 @@ def procent_trafic(data, list_for_count_trafic):
 	summ_trafic /= (1024*3)
 	procent = []
 	for i in range(len(list_for_count_trafic)-1):
-		domen_procent = (list_for_count_trafic[i][1] / round(summ_trafic, 0) * 100
-		procent.append(str(domen_procent) + '%')
+		domen_procent = str((list_for_count_trafic[i][1] / round(summ_trafic, 0)) * 100) + ' %'
+		procent.append(domen_procent)
 	return procent
 
 procent = procent_trafic(list_with_test_data, count_of_trafic)
 pd.table_trafic_domen['№', 'domen_name', 'volume_of_data', 'data_procent'] = [i for i in range(1, len(procent)+1)], count_of_trafic[0], count_of_trafic[1], procent
-
-
